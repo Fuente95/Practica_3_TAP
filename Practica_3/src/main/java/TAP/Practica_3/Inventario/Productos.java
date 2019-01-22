653bcf5f7a5f45292f14d7cb35368ddc4398b42b
Jorge <jorge.delafuente95@outlook.com>
2019-01-22 17:07:07 +0100

    Comentarios añadidos a Productos.java

--- Practica_3/src/main/java/TAP/Practica_3/Inventario/Productos.java
+++ Practica_3/src/main/java/TAP/Practica_3/Inventario/Productos.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Productos {
 
-	// Creamos las variables
+	// Creamos las variables necesarias
 	private String nombreProducto;
 	private Integer cantidadProducto;
 	private Double preciofabProducto;
@@ -23,35 +23,42 @@ public class Productos {
 	}
 	
 	// Creamos los getters y setters necesarios
+	// Getter del arraylist de los componentes del producto
 	public ArrayList <Productos> getComponentes() {
 		return componentesProducto;
 	}
 	
-	//
+	// Setter del arraylist de los componentes del producto
 	public void setComponentes (ArrayList <Productos> componentesProducto) {
 		this.componentesProducto = componentesProducto;
 	}
 	
+	// Getter del nombre del producto
 	public String getNombreProducto () {
 		return nombreProducto;
 	}
 	
+	// Setter del nombre del producto
 	public void setNombreProducto (String nombreProducto) {
 		this.nombreProducto = nombreProducto;
 	}
 	
+	// Getter de la cantidad de producto
 	public Integer getCantidadProducto () {
 		return cantidadProducto;
 	}
 	
+	// Setter de la cantidad de producto
 	public void setCantidadProducto (Integer cantidadProducto) {
 		this.cantidadProducto = cantidadProducto;
 	}
 	
+	// Getter del precio de fabricación
 	public Double getPrecioFabricacionProducto () {
 		return preciofabProducto;
 	}
 	
+	// Setter del precio de fabricación
 	public void setPrecioFabricacionProducto (Double preciofabProducto) {
 		this.preciofabProducto = preciofabProducto;
 	}
