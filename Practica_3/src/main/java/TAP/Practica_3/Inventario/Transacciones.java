c48ef4a0f06ce2a54070a6e090e3551070714085
Jorge <jorge.delafuente95@outlook.com>
2019-01-22 19:51:12 +0100

    Algunos getters y setters de Transacciones.java

--- Practica_3/src/main/java/TAP/Practica_3/Inventario/Transacciones.java
+++ Practica_3/src/main/java/TAP/Practica_3/Inventario/Transacciones.java
@@ -7,10 +7,36 @@ public class Transacciones {
 	// Declaramos algunas variables
 	private Date fechaTransaccion;
 	private Integer identifacionTransaccion;
-	private Double cantidadTransaccion;
+	private Integer cantidadTransaccion;
 	private Double costeTransaccion = 0.0;
 	
 	// Creamos el constructor
+	public Transacciones (Date fechaTransaccion, Integer identificacionTransaccion, 
+			Integer cantidadTransaccion) {
+		super();
+		this.fechaTransaccion = fechaTransaccion;
+		this.identifacionTransaccion = identificacionTransaccion;
+		this.cantidadTransaccion = cantidadTransaccion;
+	}
 	
+	// Creamos los setters y getters
+	// Getter del coste
+	public Double getCosteTransaccion() {
+		return costeTransaccion;
+	}
 	
+	// Setter del coste
+	public void setCosteTransaccion(Double costeTransaccion) {
+		this.costeTransaccion = costeTransaccion;
+	}
+	
+	// Getter de la cantidad
+	public Integer getCantidadTransaccion() {
+		return cantidadTransaccion;
+	}
+	
+	// Setter de la cantidad
+	public void setCantidadTransaccion(Integer cantidadTransaccion) {
+		this.cantidadTransaccion = cantidadTransaccion;
+	}
 }
