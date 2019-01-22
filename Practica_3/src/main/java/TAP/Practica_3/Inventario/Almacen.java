0985fa3084fb35324ba3ad4cf770b67cc83fa62a
Jorge <jorge.delafuente95@outlook.com>
2019-01-22 23:47:46 +0100

    Subida del archivo Almacen.java

---
+++ Practica_3/src/main/java/TAP/Practica_3/Inventario/Almacen.java
@@ -0,0 +1,37 @@
+package TAP.Practica_3.Inventario;
+
+import java.util.ArrayList;
+
+public class Almacen {
+
+	// Creamos algunas variables
+	private static Almacen productosAlmacen;
+	
+	// Creamos un ArrayList con los productos del inventario
+	private ArrayList <Productos> almacen = null;
+	
+	// Creamos el constructor
+	private Almacen() {
+		// Añadimos un elemento al arraylist
+		almacen = new ArrayList<Productos>();
+	}
+	
+	// Creamos la instancia
+	public static Almacen getInstance() {
+		if (productosAlmacen == null) {
+			productosAlmacen = new Almacen();
+		}
+		return productosAlmacen;
+	}
+	
+	// Creamos los getters y setters necesarios
+	// Getter de los productos que se encuentran en el almacen
+	public ArrayList<Productos> getProductosAlmacen() {
+		return almacen;
+	}
+	
+	// Setter de los productos que se encuentran en el almacen
+	public void setProductosAlmacen(ArrayList<Productos> almacen) {
+		this.almacen = almacen;
+	}	
+}
