ae1c0cb2631c1ad1e96b9888b8168c06f3a7fe1a
Jorge <jorge.delafuente95@outlook.com>
2019-01-22 17:26:14 +0100

    Creación variables Transacciones.java

---
+++ Practica_3/src/main/java/TAP/Practica_3/Inventario/Transacciones.java
@@ -0,0 +1,16 @@
+package TAP.Practica_3.Inventario;
+
+import java.sql.Date;
+
+public class Transacciones {
+
+	// Declaramos algunas variables
+	private Date fechaTransaccion;
+	private Integer identifacionTransaccion;
+	private Double cantidadTransaccion;
+	private Double costeTransaccion = 0.0;
+	
+	// Creamos el constructor
+	
+	
+}
