fd7c34f5e9eb37f178829cb8555c1e0d854ce550
Jorge <jorge.delafuente95@outlook.com>
2019-05-21 16:56:26 +0200

    Adición de un producto predeterminado más

--- Practica_3/src/main/java/TAP/Practica_3/Inventario/Predeterminado.java
+++ Practica_3/src/main/java/TAP/Practica_3/Inventario/Predeterminado.java
@@ -10,12 +10,16 @@ public class Predeterminado {
 			// Creamos algunos datos para añadir
 			String nombre1 = "Palo";
 			String nombre2 = "Acero";
+			String nombre3 = "Cuerda";
 			Double precio1 = (double) 2.6;
 			Double precio2 = (double) 1.5;
+			Double precio3 = (double) 0.3;
 			Integer cantidad1 = 3;
 			Integer cantidad2 = 2;
+			Integer cantidad3 = 6;
 			Double precioF1 = (double) 2;
 			Double precioF2 = 0.5;
+			Double precioF3 = (double) 0.2;
 			
 			// Un ArrayList para añadir los productos predeterminado
 			ArrayList<Productos> predeterminado = new ArrayList<Productos>();
@@ -29,6 +33,11 @@ public class Predeterminado {
 			Productos predeterminado2 = new Productos(nombre2, cantidad2, precio2, precioF2,
 					predeterminado);
 			Almacen.getInstance().getProductosAlmacen().add(predeterminado2);
+			
+			// Añadimos el segundo producto
+			Productos predeterminado3 = new Productos(nombre3, cantidad3, precio3, precioF3,
+					predeterminado);
+			Almacen.getInstance().getProductosAlmacen().add(predeterminado3);
 		}
 	}
 }
